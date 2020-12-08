85a5fe06bf9ad4c8df42f64ecc7d3870e431e54a
dpak1999 <deepakkumardash313@gmail.com>
2020-12-08 23:08:02 +0530

    add : portfolio data & update navabar

--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -3,19 +3,31 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavbarComponent = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">DK.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Blog</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header className="header_area">
+      <div className="main_menu">
+        <Navbar expand="lg" className="">
+          <Container className="box_1620">
+            <Navbar.Brand href="/#">
+              <span className="text-white">DK.</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="menu_nav ml-auto">
+                <Nav.Link className="text-white" href="#home">
+                  Home
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#home">
+                  About
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#home">
+                  Blog
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </header>
   );
 };
 
---
+++ src/portfolioData.js
@@ -0,0 +1,10 @@
+const home = {
+  name: "Deepak Kumar Dash",
+  title: "React JS Developer",
+  dob: "29 November 1999",
+  contact_no: "7008695909",
+  email: "dk.dash@outlook.com",
+  address: "Bhubaneswar, Odisha",
+};
+
+export { home };
